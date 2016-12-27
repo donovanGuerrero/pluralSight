cd852b9c6e662392b3f9a063ebc15c56655f86d5
dguerrero <dguerrero@DGUERRERO-WMAC>
2016-12-27 12:16:57 -0700

    Adding Files

---
+++ pluralSight/Grades/Grades/GradeBook.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades
+{
+    public class GradeBook
+    {
+
+        public GradeBook()
+        {
+            _grades = new List<float>();
+        }
+
+        public GradesStatistics ComputeStatistics()
+        {
+            GradesStatistics stats = new GradesStatistics();
+            float sum = 0;
+            foreach (float grade in _grades )
+            {
+                stats.HighestGrade = Math.Max(grade, stats.HighestGrade);
+                stats.LowestGrade = Math.Min(grade, stats.LowestGrade);
+                sum += grade;
+            }
+
+            stats.AverageGrade = sum / _grades.Count;
+
+            return stats;
+        }
+
+        public void AddGrade(float grade)
+        {
+            _grades.Add(grade);
+        }
+
+        private List<float> _grades;
+    }
+}
